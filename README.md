b6c5f34a033a8a6f8d82142c95d9250894afbe60
alberlat <alberlat@alberlats-MacBook-Air.local>
2026-04-22 14:24:55 +0800

    Publish Evolve userscript speed fork

---
+++ README.md
@@ -0,0 +1,19 @@
+# Evolve Userscript Speed Fork
+
+基于 `by22dgb/evolvescript` 的本地修改版。
+
+当前已知情况：
+
+- `20x` 以内正常使用
+- `20x` 及以上间谍任务存在 bug
+- `100x` 有概率出现卡死
+
+说明：
+
+- 这是用户脚本版本，不是完整游戏仓库
+- 高倍率属于实验功能，请自行评估风险
+- 原始项目来源：<https://github.com/by22dgb/evolvescript>
+
+文件：
+
+- `evolve_automation.user.js`
---
+++ evolve_automation.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Evolve
+// @namespace    http://tampermonkey.net/
+// @version      3.3.1.136
+// @description  try to take over the world!
+// @downloadURL  https://raw.githubusercontent.com/taczzzz/evolve-userscript-speed-fork/main/evolve_automation.user.js
+// @updateURL    https://raw.githubusercontent.com/taczzzz/evolve-userscript-speed-fork/main/evolve_automation.meta.js
+// @author       Fafnir
+// @author       TMVictor
+// @author       Vollch
+// @author       schoeggu
+// @author       davezatch
+// @author       Kewne
+// @match        https://g8hh.github.io/evolve/
+// @match        https://pmotschmann.github.io/Evolve/
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.7.1.min.js
+// @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
+// ==/UserScript==
